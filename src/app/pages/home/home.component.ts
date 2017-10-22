20fca6b25f35fd299a479f6e1e491e36d334c29f
Papa Neto <papaneto@hotmail.com>
2017-10-22 12:02:09 -0400

    estrutura e estilo formulário e frase home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css', '../../../font-awesome-4.7.0/css/font-awesome.min.css']
 })
 export class HomeComponent implements OnInit {
 
