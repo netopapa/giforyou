6a92d142f25048739a2ce344f62b2bea4e93f3b7
Papa Neto <papaneto@hotmail.com>
2017-10-22 14:14:55 -0400

    estrutura e estilo search page

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css', '../../../font-awesome-4.7.0/css/font-awesome.min.css']
 })
 export class SearchComponent implements OnInit {
 
