cfab3c3155939e23b56a9477d69397ac8ab8c721
Papa Neto <papaneto@hotmail.com>
2017-10-22 14:03:31 -0400

    rota search page adicionada

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -1,10 +1,16 @@
-import {Routes, RouterModule, RouterLinkActive} from '@angular/router';
+import {Routes, RouterModule} from '@angular/router';
+
 import {HomeComponent} from './pages/home/home.component';
+import {SearchComponent} from './pages/search/search.component';
 
 const routes: Routes = [
     {//home page
         path: '',
         component: HomeComponent
+    },
+    {//search page
+        path: 'search',
+        component: SearchComponent
     }
 ];
 
